7c5c34a1c51a73ba2c7f5dcfc137b3ca2afef607
Abhay.Nimavat <Abhay.Nimavat@fullstacklabs.digital>
2025-06-24 14:46:39 -0400

    "added support for every origin"

--- src/main/java/org/reactivestax/io/config/WebConfig.java
+++ src/main/java/org/reactivestax/io/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://<your-react-app-route>")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
